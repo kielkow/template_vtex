1366ff592cb7f8b8d6251feef83e4092bfd46c06
kielkow <matheuskiel@fiorifer.com.br>
2020-10-05 17:35:48 -0300

    Add SKU routes

--- src/modules/catalog-api/sku/index.js
+++ src/modules/catalog-api/sku/index.js
@@ -11,16 +11,39 @@ class Sku {
 
     getByRefId(refId) {
         const sku = axios.get(
-            `https://${this.credentials.accountName}.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit`,
+            `https://${this.credentials.accountName}.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitidbyrefid/${refId}`,
             {
                 headers: this.credentials,
-                params: {
-                    refId
-                }
             }
         );
 
         return sku;
+    }
+
+    getById(id) {
+        const sku = axios.get(
+            `https://${this.credentials.accountName}.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/${id}`,
+            {
+                headers: this.credentials,
+            }
+        );
+
+        return sku;
+    }
+
+    listAllSkusIds(page, pagesize) {
+        const skusIds = axios.get(
+            `https://${this.credentials.accountName}.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitids`,
+            {
+                headers: this.credentials,
+                params: {
+                    page,
+                    pagesize
+                }
+            }
+        );
+
+        return skusIds;
     }
 }
 
