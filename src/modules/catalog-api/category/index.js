51af4944aca40a6bb3c20e22a3fc861e162dcf1c
Henrique Rodrigues <henrique.c.rodrigues@hotmail.com>
2020-10-05 17:34:50 -0300

    Add module Category

---
+++ src/modules/catalog-api/category/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+class Category {
+    constructor(accountName, apiKey, appToken) {
+        this.credentials = {
+            accountName,
+            'x-vtex-api-apikey': apiKey,
+            'x-vtex-api-apptoken': appToken
+        }
+    }
+
+    getByLevels(categoryLevels = 1) {
+        const category = axios.get(
+            `https://${this.credentials.accountName}.vtexcommercestable.com.br/api/catalog_system/pub/tree/${categoryLevels}`,
+            {
+                headers: this.credentials
+            }
+        );
+
+        return category;
+    }
+}
+
+export default Category;
