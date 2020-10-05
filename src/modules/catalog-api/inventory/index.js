a791f853a119f99bcc72d4edac3a6d01b13f68d0
kielkow <matheuskiel@fiorifer.com.br>
2020-10-05 17:48:58 -0300

    Inventory routes

---
+++ src/modules/catalog-api/inventory/index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+class Inventory {
+    constructor(accountName, apiKey, appToken) {
+        this.credentials = {
+            accountName,
+            'x-vtex-api-apikey': apiKey,
+            'x-vtex-api-apptoken': appToken
+        }
+    }
+
+    updateSkuWarehouse(skuId, warehouseId, body) {
+        const sku = axios.get(
+            `https://${this.credentials.accountName}.vtexcommercestable.com.br/api/logistics/pvt/skus/${skuId}/warehouses/${warehouseId}`,
+            {
+                headers: this.credentials,
+                body,
+            }
+        );
+
+        return sku;
+    }
+}
+
+export default Inventory;
--- src/modules/vtex.js
+++ src/modules/vtex.js
@@ -1,12 +1,14 @@
 const Product = require('./catalog-api/product');
 const Sku = require('./catalog-api/sku');
 const Category = require('./catalog-api/category');
+const Inventory = require('./catalog-api/inventory');
 
 class Vtex {
     constructor(accountName, apiKey, appToken) {
         this.Product = new Product({ accountName, apiKey, appToken });
-        this.Sku = new Sku({ accountName, apiKey, appToken })
-        this.Category = new Category({ accountName, apiKey, appToken })
+        this.Sku = new Sku({ accountName, apiKey, appToken });
+        this.Category = new Category({ accountName, apiKey, appToken });
+        this.Inventory = new Inventory({ accountName, apiKey, appToken })
     }
 }
 
