825fcc0fffa90307f9aa35d5ba1124638f8e2955
kielkow <matheuskiel@fiorifer.com.br>
2020-10-06 10:27:38 -0300

    Remove params from object

--- src/modules/vtex.js
+++ src/modules/vtex.js
@@ -8,14 +8,14 @@ const StockKeepingUnit = require('./catalog-api/stockkeepingunit');
 const User = require('./orders-api/user');
 class Vtex {
     constructor(accountName, apiKey, appToken) {
-        this.Product = new Product({ accountName, apiKey, appToken });
-        this.Sku = new Sku({ accountName, apiKey, appToken });
-        this.Category = new Category({ accountName, apiKey, appToken });
-        this.Inventory = new Inventory({ accountName, apiKey, appToken });
-        this.Brand = new Brand({ accountName, apiKey, appToken });
-        this.Price = new Price({ accountName, apiKey, appToken });
-        this.StockKeepingUnit = new StockKeepingUnit({ accountName, apiKey, appToken });
-        this.User = new User({ accountName, apiKey, appToken });
+        this.Product = new Product(accountName, apiKey, appToken);
+        this.Sku = new Sku(accountName, apiKey, appToken);
+        this.Category = new Category(accountName, apiKey, appToken);
+        this.Inventory = new Inventory(accountName, apiKey, appToken);
+        this.Brand = new Brand(accountName, apiKey, appToken);
+        this.Price = new Price(accountName, apiKey, appToken);
+        this.StockKeepingUnit = new StockKeepingUnit(accountName, apiKey, appToken);
+        this.User = new User(accountName, apiKey, appToken);
     }
 }
 
