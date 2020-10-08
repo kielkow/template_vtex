f3e033417e350e36dc750a0b23b0060574aaa8d0
Henrique Rodrigues <henrique.c.rodrigues@hotmail.com>
2020-10-08 15:07:19 -0300

    New function Order(paginateLists)

---
+++ src/modules/orders-api/order/functions.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+const moment = require('moment');
+
+async function getAllOrders (credential, queryStringObject, startDate, endDate, orders = []) {
+    let urlBaseOrderList = `https://${credential.accountName}.vtexcommercestable.com.br/api/oms/pvt/orders`;
+    let queryString = Object.keys(queryStringObject).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryStringObject[key])}`).join('&');
+    let url = `${urlBaseOrderList}?${queryString}`;
+
+    let responseOrders = await axios.get(
+        url,
+        {
+            headers: credential
+        }
+    );
+    
+    let orderList = responseOrders.data.list;
+
+    if (orderList.length != 0) {
+        orders = orders.concat(orderList);
+        let pages = responseOrders.data.paging.pages;
+
+        let orderLastDateCreation = null;
+
+        if (pages > 1) {
+
+            for (let page = queryStringObject.page + 1; page <= pages; page++) {
+
+                if (page > 30) {
+                    startDate = moment(orderLastDateCreation).add(1, 'millisecond').toISOString();
+                    queryStringObject.page = 1;
+                    queryStringObject.f_creationDate = `creationDate:[${startDate} TO ${endDate}]`;
+                    orders = await getAllOrders(credential, queryStringObject, startDate, endDate, orders);
+                    break;
+                }
+
+                queryStringObject.page = page;
+                queryString = Object.keys(queryStringObject).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryStringObject[key])}`).join('&');
+                url = `${urlBaseOrderList}?${queryString}`;
+
+                responseOrders = await axios.get(
+                    url,
+                    {
+                        headers: credential
+                    }
+                );
+
+                if (responseOrders.data.list.length != 0) {
+                    orders = orders.concat(responseOrders.data.list);
+                    let order = responseOrders.data.list.pop();
+                    orderLastDateCreation = order.creationDate;
+                }
+
+            }
+        }
+    }
+
+    return orders;
+}
+module.exports = {
+    getAllOrders
+};
