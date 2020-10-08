0583c38db8857b107f299d54e363bab80d1b05fa
Henrique Rodrigues <henrique.c.rodrigues@hotmail.com>
2020-10-08 12:11:31 -0300

    Test automation and Obfuscator

---
+++ publish.js
@@ -0,0 +1,41 @@
+console.log(require('./package.json').version)
+let version = require('./package.json').version.split('.');
+version[2] = parseInt(version[2])+1;
+version = version.join('.');
+
+const {exec} = require('child_process');
+const process = require('child_process');
+
+exec(`npm version ${version}`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+    }
+
+    if (stdout) {
+        console.log(`stdout: ${stdout}`);
+    }
+
+
+});
+
+process.fork('obfuscator.js');
+
+exec(`cd dist && npm publish`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+    }
+
+    if (stdout) {
+        console.log(`stdout: ${stdout}`);
+    }
+
+
+});
